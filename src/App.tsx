93fddc53cd58d5667b60b439f5e20ec4bed065c8
Pierre Moreau <moreau.pjh@gmail.com>
2022-03-28 16:06:04 +0900

    Replace default React logo and text with simple button from atlaskit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import logo from './logo.svg'
+import Button from '@atlaskit/button'
 import './App.css'
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <Button appearance="primary">Work in progress</Button>
     </header>
   </div>
 )
